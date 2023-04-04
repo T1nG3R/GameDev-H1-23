c2655fe41bee71f714c8b7354e8cac701d3f46df
Bogdan Utenko <bohutenko@gmail.com>
2023-04-04 20:58:08 +0300

    TS-6 Player system

---
+++ Assets/Scripts/InputReader/ExternalDeviceInputReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Core.Services.Updater;
+using InputReader;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExternalDeviceInputReader : IEntityInputSource, IDisposable
+{
+    public float HorizontalDirection => Input.GetAxisRaw("Horizontal");
+    public float VerticalDirection => Input.GetAxisRaw("Vertical");
+    public bool Jump { get; private set; }
+    public bool Attack { get; private set; }
+
+    public ExternalDeviceInputReader()
+    {
+        ProjectUpdater.Instance.UpdateCalled += OnUpdate;
+    }
+
+    public void ResetOnTimeActions()
+    {
+        Jump = false;
+        Attack = false;
+    }
+
+    public void Dispose() => ProjectUpdater.Instance.UpdateCalled -= OnUpdate;
+
+    private void OnUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+            Jump = true;
+
+        if (!IsPointerOverUI() && Input.GetButtonDown("Fire1"))
+            Attack = true;
+    }
+
+    private bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
+}
--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -1,4 +1,5 @@
-using Core.Enums;
+using Core.Movement.Controller;
+using Core.Movement.Data;
 using Core.Tools;
 using Player.PlayerAnimation;
 using UnityEngine;
@@ -9,149 +10,56 @@ namespace Player
     public class PlayerEntity : MonoBehaviour
     {
         [SerializeField] private AnimatorController _animator;
-
-        [Header("HorizontalMovement")] [SerializeField]
-        private float _horizontalSpeed;
-
-        [SerializeField] private Direction _direction;
-
-        [Header("VerticalMovement")] [SerializeField]
-        private float _verticalSpeed;
-
-        [SerializeField] private float _minSize;
-        [SerializeField] private float _maxSize;
-        [SerializeField] private float _minVerticalPosition;
-        [SerializeField] private float _maxVerticalPosition;
-
-        [Header("Jump")] [SerializeField] private float _jumpForce;
-        [SerializeField] private float _gravityScale;
-        [SerializeField] private SpriteRenderer _shadow;
-        [SerializeField] [Range(0, 1)] private float _shadowSizeModificator;
-        [SerializeField] [Range(0, 1)] private float _shadowAlphaModificator;
-
+        [SerializeField] private DirectionalMovementData _directionalMovementData;
+        [SerializeField] private JumpData _jumpData;
         [SerializeField] private DirectionalCameraPair _cameras;
 
         private Rigidbody2D _rigidbody;
-
-        private float _sizeModificator;
-        private bool _isJumping;
-        private float _startJumpVerticalPosition;
-        private Vector2 _shadowLocalPosition;
-        private float _shadowVerticalPosition;
-
-        private Vector2 _movement;
+        private DirectionalMover _directionalMover;
+        private Jumper _jumper;
 
 
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
-
-            _shadowLocalPosition = _shadow.transform.localPosition;
-            float positionDifference = _maxVerticalPosition - _minVerticalPosition;
-            float sizeDifference = _maxSize - _minSize;
-            _sizeModificator = sizeDifference / positionDifference;
-            UpdateSize();
+            _directionalMover = new DirectionalMover(_rigidbody, _directionalMovementData);
+            _jumper = new Jumper(_rigidbody, _jumpData, _directionalMovementData.MaxSize);
         }
 
         private void Update()
         {
-            if (_isJumping)
-                UpdateJump();
+            if (_jumper.IsJumping)
+                _jumper.UpdateJump();
 
             UpdateAnimations();
+            UpdateCameras();
+        }
+
+        private void UpdateCameras()
+        {
+            foreach (var cameraPair in _cameras.DirectionalCameras)
+                cameraPair.Value.enabled = cameraPair.Key == _directionalMover.Direction;
         }
 
         private void UpdateAnimations()
         {
             _animator.PlayAnimation(AnimationType.Idle, true);
-            _animator.PlayAnimation(AnimationType.Walk, _movement.magnitude > 0);
-            _animator.PlayAnimation(AnimationType.Jump, _isJumping);
+            _animator.PlayAnimation(AnimationType.Walk, _directionalMover.IsMoving);
+            _animator.PlayAnimation(AnimationType.Jump, _jumper.IsJumping);
         }
 
-        public void MoveHorizontally(float direction)
-        {
-            _movement.x = direction;
-            SetDirection(direction);
-            Vector2 velocity = _rigidbody.velocity;
-            velocity.x = direction * _horizontalSpeed;
-            _rigidbody.velocity = velocity;
-        }
+        public void MoveHorizontally(float direction) => _directionalMover.MoveHorizontally(direction);
 
         public void MoveVertically(float direction)
         {
-            if (_isJumping)
+            if (_jumper.IsJumping)
                 return;
 
-            _movement.y = direction;
-            Vector2 velocity = _rigidbody.velocity;
-            velocity.y = direction * _verticalSpeed;
-            _rigidbody.velocity = velocity;
-
-            if (direction == 0)
-                return;
-
-            float verticalPosition = Mathf.Clamp(transform.position.y, _minVerticalPosition, _maxVerticalPosition);
-            _rigidbody.position = new Vector2(_rigidbody.position.x, verticalPosition);
-            UpdateSize();
+            _directionalMover.MoveVertically(direction);
         }
 
-        public void Jump()
-        {
-            if (_isJumping)
-                return;
-            _isJumping = true;
-            float jumpModificator = transform.localScale.y / _maxSize;
-            _rigidbody.AddForce(Vector2.up * (_jumpForce * jumpModificator));
-            _rigidbody.gravityScale = _gravityScale * jumpModificator;
-            _startJumpVerticalPosition = transform.position.y;
-            _shadowVerticalPosition = _shadow.transform.position.y;
-        }
-
-        private void UpdateSize()
-        {
-            float verticalDelta = _maxVerticalPosition - transform.position.y;
-            float currentSizeModificator = _minSize + _sizeModificator * verticalDelta;
-            transform.localScale = Vector2.one * currentSizeModificator;
-        }
-
-        private void SetDirection(float direction)
-        {
-            if ((_direction == Direction.Right && direction < 0) ||
-                (_direction == Direction.Left && direction > 0))
-                Flip();
-        }
-
-        private void Flip()
-        {
-            transform.Rotate(0, 180, 0);
-            _direction = _direction == Direction.Right ? Direction.Left : Direction.Right;
-            foreach (var cameraPair in _cameras.DirectionalCameras)
-                cameraPair.Value.enabled = cameraPair.Key == _direction;
-        }
-
-        private void UpdateJump()
-        {
-            if (_rigidbody.velocity.y < 0 && _rigidbody.position.y <= _startJumpVerticalPosition)
-            {
-                ResetJump();
-                return;
-            }
-
-            _shadow.transform.position = new Vector2(_shadow.transform.position.x, _shadowVerticalPosition);
-            float distance = transform.position.y - _startJumpVerticalPosition;
-            _shadow.color = new Color(0, 0, 0, 1 - distance * _shadowAlphaModificator);
-            _shadow.transform.localScale = Vector2.one * (1 + (_shadowSizeModificator * distance));
-        }
-
-        private void ResetJump()
-        {
-            _isJumping = false;
-            _shadow.transform.localPosition = _shadowLocalPosition;
-            _shadow.color = Color.black;
-            _rigidbody.position = new Vector2(_rigidbody.position.x, _startJumpVerticalPosition);
-            _rigidbody.gravityScale = 0;
-        }
-
+        public void Jump() => _jumper.Jump();
+        
         public void StartAttack()
         {
             if (!_animator.PlayAnimation(AnimationType.Attack, true))
